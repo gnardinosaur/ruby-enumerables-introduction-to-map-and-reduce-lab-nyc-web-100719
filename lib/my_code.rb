9b7c871b9558f4ca32d7b805dd4b0bab3f23a1ac
passionate-optimization-7504 <nicholasjardine9@gmail.com>
2019-09-24 12:34:43 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -38,7 +38,7 @@ def reduce_to_all_true(source_array)
   all_true = true 
   source_array.length.times { |index|
     source_array[index] = true ? all_true = true : all_true = false
-    all_true = true ? 
+    all_true = true ? next : return all_true
   }
   all_true
 end
