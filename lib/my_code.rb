287c7dfac8bec37a5b1b21128d4dddabb546b84f
passionate-optimization-7504 <nicholasjardine9@gmail.com>
2019-09-24 11:58:49 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -10,5 +10,18 @@ def map_to_no_change(source_array)
   source_array
 end
 
+def map_to_double(source_array) 
+  double_arr = []
+  source_array.length.times { |index|
+    double_arr << source_array[index] * 2 
+  }
+  double_arr 
+end 
 
-
+def map_to_square(source_array) 
+  square_arr = []
+  source_array.length.times { |index|
+    square_arr << source_array[index] ** 
+  }
+  square_arr 
+end 
